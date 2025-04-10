5eee29140f8bb326dbd9fe393ac9bf1f3ad4cde6
suwantosanjaya <suwantosanjaya@gmail.com>
2025-04-10 09:13:09 +0700

    nambah latihan2

---
+++ latihan2.php
@@ -0,0 +1,21 @@
+<?php
+    $i = 0;
+    while($i < 10){
+        echo "Selamat Datang".$i."<br>";
+        $i++;
+    }
+    
+    echo "<br>";
+
+    $j = 0;
+    do{
+        echo "Hello".$j."<br>";
+        $j++;
+    }while($j < 10);
+
+    echo "<br>";
+
+    for($k = 0; $k < 10; $k++){
+        echo "Hai".$k."<br>";
+    }
+?>
